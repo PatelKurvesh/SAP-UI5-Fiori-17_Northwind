963bece3a7fe69806f1fd6077d6530cd33253abf
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-03-05 01:25:34 +0530

    create functionality added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,32 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-17_Northwind.controller.View1", {
+		
+		onCreatePress:function(){
+			var ID = this.getView().byId("ID");
+			var name = this.getView().byId("Name");
+			var rating = this.getView().byId("Rating");
+			var price = this.getView().byId("Price");
+			var rdate = this.getView().byId("RDate");
+			var oItem = {
+				"ID":ID.getValue(),
+				"Name":name.getValue(),
+				"Rating":rating.getValue(),
+				"Price":price.getValue(),
+				"ReleaseDate":rdate.getValue()
+			};
+			
+			this.getView().getModel().create("/Products",oItem,{
+				success:function(){
+					sap.m.MessageToast.show("Created Successfully");
+				},
+				error:function(){
+					sap.m.MessageToast.show("Error");
+				}
+			});
+			
+			
+		}
 
 	});
 });
